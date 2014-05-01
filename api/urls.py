a0732778d467e18c6545b7b98e6fdc4e57686a07
yanc0 <y@nn-col.eu>
2014-05-01 13:58:19 +0200

    initial api

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, url, include
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browseable API.
+urlpatterns = patterns('',
+        url(r'^v1/', include(router.urls)),
+        url(r'^v1/auth/', include('rest_framework.urls', namespace='rest_framework'))
+)
