8ed4665b3ea1dee80d8455db433f7ccbc90b0774
renaud <bordie_r@etna-alternance.net>
2014-05-14 22:31:35 +0200

    MAJ

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Profile
+from .models import Community
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(Community)
+class CommunityAdmin(admin.ModelAdmin):
+	list_display = ['name']
