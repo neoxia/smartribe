e2d47a06e236f8c4d4e543eb44bdccd57231ec38
renaud <bordie_r@etna-alternance.net>
2014-06-08 18:32:23 +0200

    New serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group, Permission
-from core.models import Profile, Community
+from core.models import Profile, Skill, Community, Request, Offer
 from rest_framework import serializers
 
 
@@ -26,7 +26,22 @@ class ProfileSerializer(serializers.HyperlinkedModelSerializer):
         model = Profile
         fields = ('user', 'gender', 'pseudo', 'birthdate', 'bio', 'photo')
 
+class SkillSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Skill
+        fields = ('user','description')
+
 class CommunitySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Community
         fields = ('name','description')
+
+class RequestSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Request
+        fields = ('user', 'title', 'description')
+
+class OfferSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Offer
+        fields = ('request','description')
