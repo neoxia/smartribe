39d39d62b927cbce918fd1b668de2ddb0c0627f5
renaud <bordie_r@etna-alternance.net>
2014-06-14 16:15:11 +0200

    Add class Member, to allow a status inside a community

--- api/views.py
+++ api/views.py
@@ -118,6 +118,20 @@ class CommunityViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows communities to be viewed or edited.
     """
-    queryset = Community.objects.all()
+    model = Community
     serializer_class = CommunitySerializer
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+        if self.request.method == 'POST':
+            serializer_class = ProfileCreateSerializer
+        return serializer_class
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsJWTAuthenticated()]
+        elif self.request.method == 'POST':
+            return [IsJWTSelf()]
+        else:
+            return [IsJWTOwner()]
 
