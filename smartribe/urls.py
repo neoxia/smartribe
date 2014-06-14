92aa26365c6576d8f555d79b27af514a6000eb78
Yann Coleu <y@nn-col.eu>
2014-06-14 17:12:31 +0200

    Start implementing angular in django

--- smartribe/urls.py
+++ smartribe/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from smartribe import settings
 
 urlpatterns = patterns('',
     url(r'^api/', include('api.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'', 'serve', {'path': 'angular/smartribe/app/index.html'}),
+    )
