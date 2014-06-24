7f15ae0fd7217b6d01af1b0653876423a6cb8d27
Yann Coleu <y@nn-col.eu>
2014-06-24 10:34:28 +0200

    Add unittests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = '/api/v1/users/'
+        data = {
+            'username': 'test',
+            'email': 'test@test.com',
+            'password': 'pass'
+        }
+        response_data = {
+            'url': url+'1/',
+            'username': 'test',
+            'email': 'test@test.com',
+            'groups': []
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, response_data)
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from core.models import Community
 
-# Create your tests here.
+
+class CommunityTestCase(TestCase):
+    def setUp(self):
+        Community.objects.create(name="RER D")
+
+    def test_community_str(self):
+        comm = Community.objects.get(name="RER D")
+        self.assertEqual(comm.__str__(), "RER D")
