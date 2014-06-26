4704bf7ada6e125be6d4ec55bae74f6510f15326
Yann Coleu <y@nn-col.eu>
2014-06-26 12:22:22 +0200

    tests requirements et début de test unitaire community

--- api/tests/tests_community.py
+++ api/tests/tests_community.py
@@ -30,6 +30,23 @@ class CommunityTests(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+    """
+    def test_modify_community_with_auth(self):
+        # Generate token for testcommunity user
+        user = User.objects.get(username="testcommunity")
+        token = core.utils.gen_auth_token(user)
+        auth = 'JWT {0}'.format(token)
+
+        url = '/api/v1/communities/'
+        data = {
+            'description': 'com1descmodify',
+        }
+
+        response = self.client.post(url, data, HTTP_AUTHORIZATION=auth,
+                                    format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    """
+
     def test_create_community_with_auth(self):
         """
         Ensure we can create community when we are authenticated
