3cf9a99c1d53e87e4e4c1a9aa5d50bbb098e52e0
Yann Coleu <y@nn-col.eu>
2014-06-27 19:54:04 +0200

    Add community tests

--- api/tests/tests_community.py
+++ api/tests/tests_community.py
@@ -13,10 +13,6 @@ class CommunityTests(APITestCase):
         user = User(username="testcommunity", password="test")
         user.save()
 
-    def test_validate_setup(self):
-        user = User.objects.get(username='testcommunity')
-        self.assertEqual('testcommunity', user.username)
-
     def test_create_community_without_auth(self):
         """
         Ensure we cannot create community
@@ -29,23 +25,6 @@ class CommunityTests(APITestCase):
 
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-    """
-    def test_modify_community_with_auth(self):
-        # Generate token for testcommunity user
-        user = User.objects.get(username="testcommunity")
-        token = core.utils.gen_auth_token(user)
-        auth = 'JWT {0}'.format(token)
-
-        url = '/api/v1/communities/'
-        data = {
-            'description': 'com1descmodify',
-        }
-
-        response = self.client.post(url, data, HTTP_AUTHORIZATION=auth,
-                                    format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    """
 
     def test_create_community_with_auth(self):
         """
@@ -64,4 +43,35 @@ class CommunityTests(APITestCase):
 
         response = self.client.post(url, data, HTTP_AUTHORIZATION=auth,
                                     format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_modify_community_with_auth(self):
+        # Generate token for testcommunity user
+        user = User.objects.get(username="testcommunity")
+        token = core.utils.gen_auth_token(user)
+        auth = 'JWT {0}'.format(token)
+
+        url = '/api/v1/communities/1/'
+        data = {
+            'name': 'com1',
+            'description': 'com1descmodify',
+        }
+
+        response = self.client.put(url, data, HTTP_AUTHORIZATION=auth,
+                                   format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['description'], 'com1descmodify')
+
+    def test_delete_community_with_auth(self):
+        """
+        Ensure we can delete community when we are authenticated
+        """
+        # Generate token for testcommunity user
+        user = User.objects.get(username="testcommunity")
+        token = core.utils.gen_auth_token(user)
+        auth = 'JWT {0}'.format(token)
+
+        url = '/api/v1/communities/1/'
+
+        response = self.client.delete(url, HTTP_AUTHORIZATION=auth)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
