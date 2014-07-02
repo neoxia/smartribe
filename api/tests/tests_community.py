5f4da60081b1f9ebd6ab59c2a4345ab3975f5d1d
renaud <bordie_r@etna-alternance.net>
2014-07-02 21:13:45 +0200

    Unit test update

--- api/tests/tests_community.py
+++ api/tests/tests_community.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth.models import User
+from core.models import Member, Community
 import core.utils
 
 
@@ -51,7 +52,7 @@ class CommunityTests(APITestCase):
 
     def test_modify_community_with_owner(self):
         """
-        Ensure a community moderator can modify a community                     /// TO BE MODIFIED
+        Ensure a community owner can modify a community
         """
         # Generate token for community_owner user
         user = User.objects.get(username="community_owner")
@@ -81,7 +82,7 @@ class CommunityTests(APITestCase):
 
     def test_partial_modify_community_with_owner(self):
         """
-        Ensure a community moderator can modify a community                     /// TO BE MODIFIED
+        Ensure a community owner can partially modify a community
         """
         # Generate token for community_owner user
         user = User.objects.get(username="community_owner")
@@ -103,10 +104,99 @@ class CommunityTests(APITestCase):
             'description': 'com1descmodify',
         }
 
-        response = self.client.patch(url, data, HTTP_AUTHORIZATION=auth,
-                                   format='json')
+        response = self.client.patch(url, data, HTTP_AUTHORIZATION=auth, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['description'], 'com1descmodify')
+
+    def test_modify_community_without_auth(self):
+        """
+        Ensure a non authenticated user cannot modify a community
+        """
+        # Generate token for community_owner user
+        user = User.objects.get(username="community_owner")
+        token = core.utils.gen_auth_token(user)
+        auth = 'JWT {0}'.format(token)
+        # Create community
+        url = '/api/v1/communities/'
+        data = {
+            'name': 'com1',
+            'description': 'com1desc',
+        }
+        self.client.post(url, data, HTTP_AUTHORIZATION=auth, format='json')
+
+        # Create modification
+        url = '/api/v1/communities/1/'
+        data = {
+            'name': 'com1',
+            'description': 'com1descmodify',
+        }
+
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_modify_community_with_simple_user(self):
+        """
+        Ensure a simple authenticated user cannot modify a community
+        """
+        # Generate token for community_owner user
+        owner = User.objects.get(username="community_owner")
+        token = core.utils.gen_auth_token(owner)
+        auth = 'JWT {0}'.format(token)
+        # Generate token for simple_user user
+        user = User.objects.get(username="simple_user")
+        token_user = core.utils.gen_auth_token(user)
+        auth_user = 'JWT {0}'.format(token_user)
+        # Create community
+        url = '/api/v1/communities/'
+        data = {
+            'name': 'com1',
+            'description': 'com1desc',
+        }
+        self.client.post(url, data, HTTP_AUTHORIZATION=auth, format='json')
+
+        # Create modification
+        url = '/api/v1/communities/1/'
+        data = {
+            'name': 'com1',
+            'description': 'com1descmodify',
+        }
+
+        response = self.client.patch(url, data, HTTP_AUTHORIZATION=auth_user, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_modify_community_with_member(self):
+        """
+        Ensure a simple authenticated user cannot modify a community
+        """
+        # Generate token for community_owner user
+        owner = User.objects.get(username="community_owner")
+        token = core.utils.gen_auth_token(owner)
+        auth = 'JWT {0}'.format(token)
+        # Generate token for simple_user user
+        user = User.objects.get(username="simple_user")
+        token_user = core.utils.gen_auth_token(user)
+        auth_user = 'JWT {0}'.format(token_user)
+        # Create community
+        url = '/api/v1/communities/'
+        data = {
+            'name': 'com1',
+            'description': 'com1desc',
+        }
+        self.client.post(url, data, HTTP_AUTHORIZATION=auth, format='json')
+        # Create simple member
+        community = Community.objects.get(name="com1")
+        member = Member(user=user, community=community, role=2, status=1)
+        member.save()
+
+        # Create modification
+        url = '/api/v1/communities/1/'
+        data = {
+            'name': 'com1',
+            'description': 'com1descmodify',
+        }
+
+        response = self.client.patch(url, data, HTTP_AUTHORIZATION=auth_user, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
     def test_delete_community_with_owner(self):
