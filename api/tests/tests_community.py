5694d90ed42eb88584400450e33dad2a1888f99d
renaud <bordie_r@etna-alternance.net>
2014-07-02 21:26:20 +0200

    Unit test update

--- api/tests/tests_community.py
+++ api/tests/tests_community.py
@@ -198,6 +198,41 @@ class CommunityTests(APITestCase):
         response = self.client.patch(url, data, HTTP_AUTHORIZATION=auth_user, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+    def test_modify_community_with_moderator(self):
+        """
+        Ensure a simple authenticated user cannot modify a community
+        """
+        # Generate token for community_owner user
+        owner = User.objects.get(username="community_owner")
+        token = core.utils.gen_auth_token(owner)
+        auth = 'JWT {0}'.format(token)
+        # Generate token for simple_user user
+        user = User.objects.get(username="simple_user")
+        token_user = core.utils.gen_auth_token(user)
+        auth_user = 'JWT {0}'.format(token_user)
+        # Create community
+        url = '/api/v1/communities/'
+        data = {
+            'name': 'com1',
+            'description': 'com1desc',
+        }
+        self.client.post(url, data, HTTP_AUTHORIZATION=auth, format='json')
+        # Create simple member
+        community = Community.objects.get(name="com1")
+        member = Member(user=user, community=community, role=1, status=1)
+        member.save()
+
+        # Create modification
+        url = '/api/v1/communities/1/'
+        data = {
+            'name': 'com1',
+            'description': 'com1descmodify',
+        }
+
+        response = self.client.patch(url, data, HTTP_AUTHORIZATION=auth_user, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['description'], 'com1descmodify')
+
 
     def test_delete_community_with_owner(self):
         """
@@ -207,8 +242,15 @@ class CommunityTests(APITestCase):
         user = User.objects.get(username="community_owner")
         token = core.utils.gen_auth_token(user)
         auth = 'JWT {0}'.format(token)
+        # Create community
+        url = '/api/v1/communities/'
+        data = {
+            'name': 'com1',
+            'description': 'com1desc',
+        }
+        self.client.post(url, data, HTTP_AUTHORIZATION=auth, format='json')
 
         url = '/api/v1/communities/1/'
 
         response = self.client.delete(url, HTTP_AUTHORIZATION=auth)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
