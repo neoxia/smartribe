80423973a589a737e14cc9275da1b3e713bf1a95
renaud <bordie_r@etna-alternance.net>
2014-07-16 12:00:37 +0200

    Refactoring

--- api/views/media.py
+++ api/views/media.py
@@ -14,6 +14,18 @@ class MediaViewSet(viewsets.ViewSet):
 
     @action(methods=['GET', ])
     def get_media(request, path, document_root=None):
+        """ Get media:
+
+                | **permission**: authenticated
+                | **endpoint**: /media/path/
+                | **method**: GET
+                | **http return**:
+                |       - 200 OK
+                |       - 404 Not allowed
+                | **data return**:
+                |       - bytecode
+
+        """
         #response = HttpResponse()
         #image = Image.open(path)
         #image.save(response, 'PNG')
