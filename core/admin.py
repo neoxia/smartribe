072f4b56a91246bf76127417f9d041d0abaa060b
Yann Coleu <y@nn-col.eu>
2014-07-22 22:30:49 +0200

    Multitable inheritance for communities

--- core/admin.py
+++ core/admin.py
@@ -5,51 +5,80 @@ from core.models.address import Address
 from core.models.skill import SkillCategory
 from .models import Profile
 from .models import Community
+from .models import TransportCommunity
+from .models import LocalCommunity
+from .models import TransportStop
 from .models import Tos
 from .models import Skill
 from .models import Request
 from .models import Offer
 from .models import PasswordRecovery
 
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user']
+
 
 @admin.register(Community)
 class CommunityAdmin(admin.ModelAdmin):
     list_display = ['name']
 
+
+@admin.register(TransportCommunity)
+class TransportCommunityAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(LocalCommunity)
+class LocalCommunityAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(TransportStop)
+class TransportStopAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = ['user', 'community']
+
 
 @admin.register(Tos)
 class TosAdmin(admin.ModelAdmin):
     list_display = ['version']
 
+
 @admin.register(SkillCategory)
 class SkillCategoryAdmin(admin.ModelAdmin):
     list_display = ['name']
+
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     list_display = ['user']
 
+
 @admin.register(Request)
 class RequestAdmin(admin.ModelAdmin):
     list_display = ['title']
+
 
 @admin.register(Offer)
 class OfferAdmin(admin.ModelAdmin):
     list_display = ['request']
 
+
 @admin.register(PasswordRecovery)
 class PasswordRecoveryAdmin(admin.ModelAdmin):
     list_display = ['user']
 
+
 @admin.register(ActivationToken)
 class ActivationTokenAdmin(admin.ModelAdmin):
     list_display = ['user']
+
 
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
--- core/models/community.py
+++ core/models/community.py
@@ -1,21 +1,13 @@
 from django.db import models
+from core.models.address import Address
 
 
 class Community(models.Model):
-
-    name = models.CharField(max_length=50,
-                            unique=True)
-
+    name = models.CharField(max_length=50, unique=True)
     description = models.TextField(max_length=180)
-
-    banner = models.ImageField(blank=True,
-                               null=True)
-
-    logo = models.ImageField(blank=True,
-                             null=True)
-
+    banner = models.ImageField(blank=True, null=True)
+    logo = models.ImageField(blank=True, null=True)
     creation_date = models.DateField(auto_now_add=True)
-
     auto_accept_member = models.BooleanField(default=False)
 
     def __str__(self):
@@ -24,4 +16,37 @@ class Community(models.Model):
     class Meta:
         verbose_name = 'community'
         verbose_name_plural = 'communities'
+        app_label = 'core'
+
+
+class TransportStop(models.Model):
+    name = models.CharField(max_length=50)
+    detail = models.TextField(max_length=180, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'transport stop'
+        verbose_name_plural = 'transport stops'
+        app_label = 'core'
+
+
+class TransportCommunity(Community):
+    transport_stop_departure = models.ForeignKey(TransportStop,
+                                                 related_name='departure')
+    transport_stop_via = models.ForeignKey(TransportStop,
+                                           related_name='via')
+    transport_stop_arrival = models.ForeignKey(TransportStop,
+                                               related_name='arrival')
+
+    class Meta:
+        verbose_name = 'transport community'
+        verbose_name_plural = 'transport communities'
+        app_label = 'core'
+
+
+class LocalCommunity(Community):
+    address = models.ForeignKey(Address)
+
+    class Meta:
+        verbose_name = 'local community'
+        verbose_name_plural = 'local communities'
         app_label = 'core'
