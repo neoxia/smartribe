291b48d3b7aad50e97f50915fb643216688d76ba
renaud <bordie_r@etna-alternance.net>
2014-11-17 22:01:20 +0100

    Settings

--- smartribe/settings.py
+++ smartribe/settings.py
@@ -91,23 +91,13 @@ WSGI_APPLICATION = 'smartribe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'NAME': 'smartribe',
-            'ENGINE': 'django.db.backends.mysql',
-            'USER': 'smartribe',
-            'PASSWORD': 'password',
-            'HOST': '10.129.235.136',
-        }
-    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
