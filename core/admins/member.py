e2d49897b3591d72bccb3ace20ac2ddda895efee
renaud <bordie_r@etna-alternance.net>
2015-01-21 21:49:18 +0100

    Administration interface upgrade

--- core/admins/member.py
+++ core/admins/member.py
@@ -5,5 +5,6 @@ from core.models import Member
 class MemberAdmin(BasicAdmin):
     model = Member
     list_display = ['user', 'community', 'role', 'status', 'id']
+    list_editable = ['role', 'status']
     search_fields = ['user__username', 'community__name']
     list_filter = ['role', 'status']
